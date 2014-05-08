27e136ad9843a29fa62d5e275aeef8535bf976ab
Oleh Kravchenko <oleg@kaa.org.ua>
2014-05-08 21:45:16 +0300

    Implement edit functions
    Added copy, cut, paste, undo and redo slots;
    Added new, open, save and save as slots;
    Added m_Changed flag.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,10 +5,12 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    m_Changed(false)
 {
     ui->setupUi(this);
-    this->setCentralWidget(ui->textEdit);
+
+    QObject::connect(ui->textEdit, SIGNAL(textChanged()), this, SLOT(on_Changed()));
 }
 
 MainWindow::~MainWindow()
@@ -16,21 +18,134 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_Changed()
+{
+    m_Changed = true;
+}
+
+/* menu file */
+
 void MainWindow::on_ActionNew_triggered()
 {
-    ;
+    if(m_Changed) {
+        QMessageBox msgBox;
+
+        msgBox.setText("The document has been modified.");
+        msgBox.setInformativeText("Do you want to save your changes?");
+        msgBox.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        msgBox.setDefaultButton(QMessageBox::Save);
+
+        switch (msgBox.exec()) {
+            case QMessageBox::Save:
+                on_ActionSave_triggered();
+                break;
+
+            case QMessageBox::Cancel:
+                return;
+
+            default:
+                break;
+        }
+    }
+
+    ui->textEdit->clear();
+    m_Filename.clear();
+    m_Changed = false;
 }
 
 void MainWindow::on_ActionOpen_triggered()
 {
-    ;
+    on_ActionNew_triggered();
+
+    QString filename = QFileDialog::getOpenFileName(this);
+
+    if (!filename.isEmpty()) {
+        QFile f(filename);
+
+        if (f.open(QFile::ReadOnly | QFile::Text)) {
+            QTextStream in(&f);
+
+            ui->textEdit->setPlainText(in.readAll());
+
+            m_Filename = filename;
+        }
+    }
 }
+
+void MainWindow::on_ActionSave_triggered()
+{
+    if (m_Filename.isEmpty()) {
+        on_ActionSave_as_triggered();
+
+        return;
+    }
+
+    QFile f(m_Filename);
+
+    if (f.open(QFile::WriteOnly | QFile::Text)) {
+        QTextStream out(&f);
+
+        out << ui->textEdit->toPlainText();
+
+        m_Changed = false;
+    }
+}
+
+void MainWindow::on_ActionSave_as_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this);
+
+    if (!filename.isEmpty()) {
+        QFile f(filename);
+
+        if (f.open(QFile::WriteOnly | QFile::Text)) {
+            QTextStream out(&f);
+
+            out << ui->textEdit->toPlainText();
+
+            m_Filename = filename;
+            m_Changed = false;
+        }
+    }
+}
+
+/* menu edit*/
+
+void MainWindow::on_ActionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_ActionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_ActionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_ActionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_ActionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+/* menu help */
 
 void MainWindow::on_ActionAbout_triggered()
 {
     QMessageBox msgBox(this);
 
-    msgBox.windowTitle() = "QuickPad";
-    msgBox.setText("(c) "__DATE__ " QuickPad <oleg@kaa.org.ua>");
+    msgBox.setText(
+        "(c) QuickPad Oleh Kravchenko <oleg@kaa.org.ua>\n"
+        "Build date: " __DATE__
+    );
+
     msgBox.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,14 +16,40 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_Changed();
+
+    /* menu file */
+
     void on_ActionNew_triggered();
 
     void on_ActionOpen_triggered();
+
+    void on_ActionSave_triggered();
+
+    void on_ActionSave_as_triggered();
+
+    /* menu edit */
+
+    void on_ActionCopy_triggered();
+
+    void on_ActionCut_triggered();
+
+    void on_ActionPaste_triggered();
+
+    void on_ActionUndo_triggered();
+
+    void on_ActionRedo_triggered();
+
+    /* menu help */
 
     void on_ActionAbout_triggered();
 
 private:
     Ui::MainWindow *ui;
+
+    QString m_Filename;
+
+    bool m_Changed;
 };
 
 #endif // MAINWINDOW_H
